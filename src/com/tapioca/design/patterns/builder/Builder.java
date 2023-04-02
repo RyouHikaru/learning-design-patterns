a5102e8330bdb1c743b24fccdfa3263863384e53
RyouHikaru <carljamesreblora13@gmail.com>
2023-04-02 13:24:54 +0800

    Added Builder

---
+++ src/com/tapioca/design/patterns/builder/Builder.java
@@ -0,0 +1,116 @@
+package com.tapioca.design.patterns.builder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Builder - allows you to build piece by piece
+ */
+class HtmlElement {
+    public String name;
+    public String text;
+    public ArrayList<HtmlElement> elements = new ArrayList<>();
+    private final int indentSize = 2;
+    private final String newLine = System.lineSeparator();
+
+    public HtmlElement() {
+
+    }
+
+    public HtmlElement(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+
+    private String toStringImpl(int indent) {
+        StringBuilder sb = new StringBuilder();
+        String i = String.join("", Collections.nCopies(indent * indentSize, " "));
+        sb.append(String.format("%s<%s>%s", i, name, newLine));
+        if (text != null && !text.isEmpty()) {
+            sb.append(String.join("", Collections.nCopies(indentSize * (indent + 1), " ")))
+                    .append(text)
+                    .append(newLine);
+        }
+
+        for (HtmlElement e : elements)
+            sb.append(e.toStringImpl(indent + 1));
+
+        sb.append(String.format("%s</%s>%s", i, name, newLine));
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return toStringImpl(0);
+    }
+}
+
+
+class HtmlBuilder {
+    private String rootName;
+    private HtmlElement root = new HtmlElement();
+
+    public HtmlBuilder(String rootName) {
+        this.rootName = rootName;
+        root.name = rootName;
+    }
+
+    // Not fluent
+    public void addChild(String childName, String childText) {
+        HtmlElement e = new HtmlElement(childName, childText);
+        root.elements.add(e);
+    }
+
+    public HtmlBuilder addChildFluent(String childName, String childText) {
+        HtmlElement e = new HtmlElement(childName, childText);
+        root.elements.add(e);
+        return this;
+    }
+
+    public void clear() {
+        root = new HtmlElement();
+        root.name = rootName;
+    }
+
+    // Delegating
+    @Override
+    public String toString() {
+        return root.toString();
+    }
+}
+
+public class Builder {
+    public static void main(String[] args) {
+        // We want to build a simple HTML paragraph
+        System.out.println("Testing");
+        String hello = "hello";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<p>")
+                .append(hello)
+                .append("</p>"); // a builder!
+        System.out.println(sb);
+
+         // Now we want to build a list with 2 words
+        String[] words = {"hello", "world"};
+        sb.setLength(0); // clear it
+        sb.append("<ul>\n");
+        for (String word : words) {
+            // Indentation management, line breaks and other evils
+            sb.append(String.format("  <li>%s</li>\n", word));
+        }
+        sb.append("</ul>");
+        System.out.println(sb);
+
+        // Ordinary non-fluent builder
+        HtmlBuilder builder = new HtmlBuilder("ul");
+        builder.addChild("li", "hello");
+        builder.addChild("li", "world");
+        System.out.println(builder);
+
+        // Fluent builder
+        builder.clear();
+        builder.addChildFluent("li", "hello")
+                .addChildFluent("li", "world");
+        System.out.println(builder);
+    }
+}
---
+++ src/com/tapioca/design/patterns/builder/recursive/RecursiveBuilder.java
@@ -0,0 +1,53 @@
+package com.tapioca.design.patterns.builder.recursive;
+
+class Person {
+    public String name;
+    public String position;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
+
+class PersonBuilder<SELF extends PersonBuilder<SELF>> {
+    protected Person person1 = new Person();
+
+    public SELF withName(String name) {
+        person1.name = name;
+        return (SELF) this;
+    }
+
+    protected SELF self() {
+        return (SELF) this;
+    }
+
+    public Person build() {
+        return person1;
+    }
+}
+
+class EmployeeBuilder extends PersonBuilder<EmployeeBuilder> {
+    public EmployeeBuilder worksAt(String position) {
+        person1.position = position;
+        return self();
+    }
+
+    @Override
+    protected EmployeeBuilder self() {
+        return this;
+    }
+}
+
+public class RecursiveBuilder {
+    public static void main(String[] args) {
+        EmployeeBuilder eb = new EmployeeBuilder();
+        Person tapioca = eb.withName("Tapioca")
+                .worksAt("Developer")
+                .build();
+        System.out.println(tapioca);
+    }
+}
